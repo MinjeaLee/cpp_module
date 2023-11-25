ab0d4a89e1816819e2b76ff0f6a83246845d10ce
LeeMinjea <gabe0529@naver.com>
2023-11-26 00:14:34 +0900

    04

---
+++ cpp_module_04/ex02/Brain.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class Brain
+{
+private:
+	std::string ideas[100];
+public:
+	Brain();
+	Brain(const Brain &copy);
+	Brain &operator=(const Brain &copy);
+	~Brain();
+	std::string getIdea(int index) const;
+	void setIdea(int index, const std::string &idea);
+};
